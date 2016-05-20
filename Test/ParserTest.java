edc50515389f094d4bb15f614b3d4fcb55e2488a
Blynk <david.obatake@gmail.com>
2016-05-20 16:02:39 -0700

    test harness for parser

--- Test/ParserTest.java
+++ Test/ParserTest.java
@@ -2,6 +2,7 @@ import org.jsoup.Jsoup;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by dobatake on 5/19/16.
@@ -10,6 +11,9 @@ public class ParserTest {
     public static void main(String[] args) {
         try {
             DetailParser parser = new DetailParser(Jsoup.parse(new File("test_html.htm"), "UTF-8"));
+            for(Map.Entry<String, String> entry: parser.detailAttrs.entrySet()){
+                System.out.println(entry);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
