27d98603859cd3db6cfde8b4399df78f6de7d741
betodt <alberto.diaz.tostado@gmail.com>
2016-05-20 20:08:30 -0700

    bootstrapped angular

---
+++ js/controllers/MainController.js
@@ -0,0 +1,14 @@
+app.controller('myCtrl', function($scope) {
+	$scope.results = [
+		{
+			link: 'http://images.craigslist.org/00S0S_PPirCqKK4f_600x450.jpg',
+			title: 'Lovely 1 bedroom apartment great location mature landscapping',
+			price: '$2050',
+			br: '1',
+			ba: '1',
+			sqFt: '1356',
+			city: 'cupertino',
+			avail: 'available jun 01'
+		}
+	];
+});
